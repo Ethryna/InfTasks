a3f3026c51841306e57d4e1be4fe48bedbd9317a
Ethryna <114387952+Ethryna@users.noreply.github.com>
2022-11-07 15:13:47 +0700

    Add files via upload

---
+++ EGE_15_2023Demo.py
@@ -0,0 +1,11 @@
+for a in range(1,1000):
+    Fh = True
+    for x in range(1, 100000):
+        if ((x % 2 == 0) <= (x % 3 != 0)) or (x + a >= 100):
+            Fh = True
+        else: 
+            Fh = False
+            break
+    if Fh == True:
+        print(a)
+        exit()
