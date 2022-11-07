bc3b05a93ff244ce6343898e0f7168ec549f11b0
Ethryna <114387952+Ethryna@users.noreply.github.com>
2022-11-07 15:26:14 +0700

    Update EGE_15_2023Demo.py

--- EGE_15_2023Demo.py
+++ EGE_15_2023Demo.py
@@ -1,6 +1,6 @@
 for a in range(1,1000):
     Fh = True
-    for x in range(1, 100000):
+    for x in range(1, 10000):
         if ((x % 2 == 0) <= (x % 3 != 0)) or (x + a >= 100):
             Fh = True
         else: 
