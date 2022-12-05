7992f22f916f9d5e21977e6ea760ff1a0db5e07b
Ethryna <114387952+Ethryna@users.noreply.github.com>
2022-12-05 16:07:59 +0700

    Add files via upload

--- EGE_9_2023Demo.py
+++ EGE_9_2023Demo.py
@@ -1,11 +1,11 @@
 tabl = open('C:/Users/student/Documents/142Б/9.csv')
 nums = []
-ValL = []
 numRep = 0
 c = 0
+r = 0
+numsF = []
 RESLINES = 0
 summ = 0
-More = False
 EOF = False
 while not EOF:
     s = tabl.readline()
@@ -16,25 +16,30 @@ while not EOF:
         nums = (s[:len(s)-1].split(';'))
         for i in range(6):
             for j in range(6):
-                if nums[i]==nums[j]:
-                    if not c > 2:
-                        c+=1
-                        numRep = nums[i]
-                    else:
-                        More = True
-                        break
-                c = 0
-        if More == False:
-            ValL.append(nums)
-        else: More = False
-        for k in nums:
-            if k != numRep:
-                summ += int(k)
-            if summ/4 <= 2*int(numRep):
-                RESLINES += 1
+                if nums[i]==nums[j] and i != j and numRep == 0:
+                    numRep = nums[i]
+                    c = 1
+                elif nums[i]==nums[j] and i != j and numRep != 0 and nums[i] != numRep:
+                    c = 2
+                    break
+        if c == 1:
+            for i in range(6):
+                for j in range(6):
+                    if nums[i]==nums[j] and i != j:
+                        r += 1
+            if r == 2:
+                for k in nums:
+                    if k != numRep:
+                        summ+=int(k)
+                if summ/4 <= 2*int(numRep):
+                    RESLINES += 1
         nums = []
+        numsF = []
         summ = 0
         numRep = 0
+        c = 0
+        r = 0
+
 print(RESLINES)
 
 
