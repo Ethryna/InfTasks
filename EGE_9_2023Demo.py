b2353f1fff086ca0978a98167d960e8419e35780
Ethryna <114387952+Ethryna@users.noreply.github.com>
2022-12-12 14:48:25 +0700

    Update EGE_9_2023Demo.py

--- EGE_9_2023Demo.py
+++ EGE_9_2023Demo.py
@@ -1,9 +1,7 @@
 tabl = open('C:/Users/student/Documents/142Б/9.csv')
 nums = []
 numRep = 0
-c = 0
 r = 0
-numsF = []
 RESLINES = 0
 summ = 0
 EOF = False
@@ -16,29 +14,21 @@ while not EOF:
         nums = (s[:len(s)-1].split(';'))
         for i in range(6):
             for j in range(6):
-                if nums[i]==nums[j] and i != j and numRep == 0:
-                    numRep = nums[i]
-                    c = 1
-                elif nums[i]==nums[j] and i != j and numRep != 0 and nums[i] != numRep:
-                    c = 2
-                    break
-        if c == 1:
-            for i in range(6):
-                for j in range(6):
-                    if nums[i]==nums[j] and i != j:
-                        r += 1
-            if r == 2:
-                for k in nums:
-                    if k != numRep:
-                        summ+=int(k)
+                for i in range(6):
+                    if i!=j:
+                        if nums[i]==nums[j]:
+                            numRep = nums[i]
+                            r+=1
+            if r==2:
+                for k in list(map(int,nums)):
+                    if k!=int(numRep):
+                        summ+=k
                 if summ/4 <= 2*int(numRep):
-                    RESLINES += 1
+                    RESLINES+=1
         nums = []
-        numsF = []
         summ = 0
         numRep = 0
-        c = 0
         r = 0
-
+        
 print(RESLINES)
 tabl.close()
