cae9c9e8d0624be3ccab998281ef13306c458615
Ethryna <114387952+Ethryna@users.noreply.github.com>
2023-02-09 10:06:02 +0700

    Update progs.py

--- 2 полугодие/progs.py
+++ 2 полугодие/progs.py
@@ -1,32 +1,36 @@
 from itertools import *
-def f(x,y):
+def f1(x,y):
     count = 0
-    for i in range(1,y-x+1):
+    for i in range(1,10):
         progs = list(product('12',repeat=i))
-        t = x
         for k in progs:
             k1 = ''.join(k)
+            t = x
             for c in k1:
                 if c =='1': t+=1
                 else: t*=2
                 if t > y or t==17: break
-                elif t == y: count+=1
+            if t == y: count+=1
     return(count)
-r1 = f(1,10)
-print(r1)
-r2 = f(10,35)
-print(r2)
-"""for x in range(1,26):
-    progs = product('12',repeat=x)
-    t = 10
-    for k in progs:
-        k1 = ''.join(k)
-        for c in k1:
-            if c =='1': t+=1
-            else: t*=2
-            if t > 35 or t==17: break
-            elif t == 35: count2+=1
-            if t == 10: f10 = True
-            if t == 17 or t > 35:
-                break
-            if t == 35: count+=1"""
+
+def f2(x,y):
+    count = 0
+    for i in range(1,24):
+        progs = list(product('12',repeat=i))
+        for k in progs:
+            k1 = ''.join(k)
+            t = x
+            if k1.count('2')>1:
+                continue
+            else:
+                for c in k1:
+                    if c =='1': t+=1
+                    else: t*=2
+                    if t > y or t==17: break
+                if t == y: count+=1
+    return(count)
+r1 = f1(1,10)
+r2 = f2(10,35)
+print(r1*r2)
+
+#Ужасное задание
